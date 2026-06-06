665cae9d26b377acad263a404fb20f6bf3c2c0a7
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-06 15:23:18 +0530

    Dependency Injection

--- src/main/java/com/example/store/OrderService.java
+++ src/main/java/com/example/store/OrderService.java
@@ -1,16 +1,23 @@
 package com.example.store;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder() {
-        PaymentService paymentService = new StripePaymentService();
+        paymentService = new PaypalPaymentService();
         paymentService.processPayment(1000);
     }
-}
 
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+}
---
+++ src/main/java/com/example/store/PaypalPaymentService.java
@@ -0,0 +1,14 @@
+package com.example.store;
+
+import org.springframework.stereotype.Service;
+
+@Service("paypal")
+public class PaypalPaymentService implements PaymentService {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Paypal");
+        System.out.println("Amount: " + amount);
+    }
+
+}
