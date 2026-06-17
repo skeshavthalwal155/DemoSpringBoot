628c87ef422a080d0f64e6fbf74a6dfd81b4b670
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-17 18:30:39 +0530

    updated code

--- src/main/java/com/example/store/PaymentService.java
+++
@@ -1,6 +0,0 @@
-package com.example.store;
-
-public interface PaymentService {
-    void processPayment(double amount);
-
-}
--- src/main/java/com/example/store/StoreApplication.java
+++ src/main/java/com/example/store/StoreApplication.java
@@ -2,15 +2,11 @@ package com.example.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-
-        var manager = context.getBean(OrderService.class);
-        manager.placeOrder();
+        SpringApplication.run(StoreApplication.class, args);
     }
 }
