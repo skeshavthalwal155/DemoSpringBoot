c05d1c0be68d07feea1dcb0bd9e9ed9a8f9c7bdb
skeshavthalwal155 <keshavthalwal155@gmail.com>
2026-06-23 19:18:11 +0530

    creating wishlist table

--- src/main/java/com/example/store/entities/User.java
+++ src/main/java/com/example/store/entities/User.java
@@ -69,6 +69,15 @@ public class User {
     @OneToOne(mappedBy = "user")
     private Profile profile;
 
+    @ManyToMany
+    @JoinTable(
+            name = "wishlist",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+
+    )
+    private Set<Product> wishlist = new HashSet<>();
+
 
 
 }
